8b05fa464bb4ef97c7855d24d670e4b9c6698bfd
felipedev418 <felipedeaalmeida@gmail.com>
2017-05-05 15:11:38 +0100

    Show Question if "Multiple Choice Question" OR (Good answers is 1 AND "Unique Choice Question")

--- includes/templates/question-content.php
+++ includes/templates/question-content.php
@@ -9,37 +9,44 @@ $answers = get_post_meta($question->ID,'answers', true);
 
 $goods = get_post_meta($question->ID,'goods', true);
 
-?>
-<h2><?php echo esc_html($question->post_content); ?></h2>
-<ul class="answers">
-	<?php foreach ($answers as $key => $answer) { ?>
-			<?php 
-				if( 
-					( 
-					(is_array($qst_user_ansers) && in_array($key, $qst_user_ansers) ) || (!is_array($qst_user_ansers) && $key == $qst_user_ansers ) )
-					 && array_key_exists($key, $goods)
-					 ){
-					$choice_eval = '<span style="color:green;">Good choice</span>';
-				}else if( 
-					(
-						(is_array($qst_user_ansers) && in_array($key, $qst_user_ansers) ) || ( !is_array($qst_user_ansers) && $key ==$qst_user_ansers ) )
-					&& !array_key_exists($key, $goods) 
-					){
-					$choice_eval = '<span style="color:red;">Bad choice</span>';
-				}else{
-					$choice_eval = '';
-				}
-			?>
-			<li>
-				<label><?php echo  $choice_eval; ?>
-					<?php if( 'ucq' == $quiz_type ): ?>
-						<input type="radio" name="answer[<?php echo $question->ID; ?>]" value="<?php echo $key; ?>">
-					<?php else: ?>
-						<input type="checkbox" name="answer[<?php echo $question->ID; ?>][<?php echo $key; ?>]" value="<?php echo $key; ?>">
-				<?php endif; ?>
-					<span><?php echo esc_html($answer) ; ?></span>
-				</label>
-			</li>
-	<?php } ?>
-</ul>
-<input type="hidden" name="questions[]" value="<?php echo $question->ID; ?>">
+// Show question only if there is one good answer at least
+if ( count($goods) > 0 ) {
+	// show if Multiple Choice Question OR (Good answers is 1 AND Unique Choice Question)
+	if( ('mcq' == $quiz_type) || (count($goods) == 1 && 'ucq' == $quiz_type) ){
+		?>
+		<h2><?php echo esc_html($question->post_content); ?></h2>
+		<ul class="answers">
+			<?php foreach ($answers as $key => $answer) { ?>
+					<?php 
+						if( 
+							( 
+							(is_array($qst_user_ansers) && in_array($key, $qst_user_ansers) ) || (!is_array($qst_user_ansers) && $key == $qst_user_ansers ) )
+							 && array_key_exists($key, $goods)
+							 ){
+							$choice_eval = '<span style="color:green;">Good choice</span>';
+						}else if( 
+							(
+								(is_array($qst_user_ansers) && in_array($key, $qst_user_ansers) ) || ( !is_array($qst_user_ansers) && $key ==$qst_user_ansers ) )
+							&& !array_key_exists($key, $goods) 
+							){
+							$choice_eval = '<span style="color:red;">Bad choice</span>';
+						}else{
+							$choice_eval = '';
+						}
+					?>
+					<li>
+						<label><?php echo  $choice_eval; ?>
+							<?php if( 'ucq' == $quiz_type ): ?>
+								<input type="radio" name="answer[<?php echo $question->ID; ?>]" value="<?php echo $key; ?>">
+							<?php else: ?>
+								<input type="checkbox" name="answer[<?php echo $question->ID; ?>][<?php echo $key; ?>]" value="<?php echo $key; ?>">
+						<?php endif; ?>
+							<span><?php echo esc_html($answer) ; ?></span>
+						</label>
+					</li>
+			<?php } ?>
+		</ul>
+		<input type="hidden" name="questions[]" value="<?php echo $question->ID; ?>">
+		<?php
+	}
+}
